Fixed the encryption function of hill.py. Still trouble though if the key matrix dimension is not a factor of the phrase length.

Hill/hill.py
@@ -8,12 +8,11 @@ from colorama import init
 init(autoreset=True)
 from colorama import Fore, Back, Style
 
-
-
 def encrypt():
 	# Define variables
 	alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 	numericPhrase = []
+	appendedList = []
 	counter = 0
 	check = 0
 
@@ -22,20 +21,15 @@ def encrypt():
 	phrase =  list(phrase)
 	print(phrase)
 
-	# Get matrix dimensions
-	# dimension = int(input("Enter integer key dimension: "))
+	# Get key dimensions   --   ************** UPDATE PRIINT MESSAGE WHEN FIXED ***************
+	print(Back.YELLOW + Style.BRIGHT +"As of 7/16/2018 (M/D/Y), key dimension must be a factor or the phrase length. Will fix soon.")
+	keyDimension = int(input("Enter key dimension (Only one value as key is a square matrix): "))
 
-	# # Split phrase
-	# def chunk(it, size):
-	#     it = iter(it)
-	#     return iter(lambda: tuple(islice(it, size)), ())
-
-	# phrase = list(chunk(phrase, 3))
-	# print(phrase)
-
+	# Generate random key
 	while (check == 0):
-		randomMatrix = np.random.randint(low = 0, high = 25, size = (len(phrase),len(phrase)))
+		randomMatrix = np.random.randint(low = 0, high = 25, size = (keyDimension,keyDimension))
 		key = np.matrix(randomMatrix)
+		print(Fore.GREEN + Style.BRIGHT + "Randomly generated key:")
 		print(key)
 
 		# Check if determinant is nonzero 
@@ -46,7 +40,7 @@ def encrypt():
 			break
 
 	while (counter < 26):
-		numericPhrase.append(alphabet.index(phrase[counter]))
+		numericPhrase.append(int(alphabet.index(phrase[counter])))
 		counter = counter + 1
 		if (len(numericPhrase) ==  len(phrase)):
 			counter = 0
@@ -55,30 +49,62 @@ def encrypt():
 	print(Fore.GREEN + Style.BRIGHT + "Numeric Phrase:")
 	print(numericPhrase)
 
-	# Convert phrase to matrix
-	phraseArray = np.reshape(numericPhrase, (len(phrase), 1))
-	phraseMatrix = np.matrix(phraseArray)
+	# Split phrase
+	def chunk(it, size):
+	    it = iter(it)
+	    return iter(lambda: tuple(islice(it, size)), ())
 
-	# Multiply key and phrase matrices
-	encipheredVector = key * phraseMatrix 
-	print(Fore.GREEN + Style.BRIGHT + "Enciphered vector:")
-	print(encipheredVector)
+	numericPhrase = list(chunk(numericPhrase, keyDimension))
+	print(numericPhrase)
+	segments = len(numericPhrase)
+	counter = 0
 
-	# Modulus
-	final = np.remainder(encipheredVector, 26)
-	print(Fore.GREEN + Style.BRIGHT + "Modulated:")
-	print(final)
+	while (counter < segments):
+		print(Fore.GREEN + Style.BRIGHT + "------------------------------")
+		a = list(numericPhrase[counter])
+		print(a)
+		counter = counter + 1
 
-	# Convert back to text
-	finalNumeric = final.tolist()
-	merged = list(itertools.chain(*finalNumeric))
-	print(merged)
+		# Convert phrase to matrix
+		phraseArray = np.reshape(a, (keyDimension, 1))
+		phraseMatrix = np.matrix(phraseArray)
 
+		print(phraseMatrix)
+
+		# Multiply key and phrase matrices
+		encipheredVector = key * phraseMatrix 
+		print(Fore.GREEN + Style.BRIGHT + "Enciphered vector:")
+		print(encipheredVector)
+
+		# Modulus
+		final = np.remainder(encipheredVector, 26)
+		print(Fore.GREEN + Style.BRIGHT + "Modulated:")
+		print(final)
+
+		# Convert back to text
+		finalNumeric = final.tolist()
+		merged = list(itertools.chain(*finalNumeric))
+		print(merged)
+
+		appendedList.append(merged)
+
+
+	print(Fore.GREEN + Style.BRIGHT + "------------------------------")
+
+	print(appendedList)
 	finalLetters = []
 	counter = 0
 
+	total = []
+	for i in appendedList:
+		total += i
+
+	print(total)
+
+
+
 	while (counter < len(phrase)):
-		finalLetters.append(alphabet[merged[counter]])
+		finalLetters.append(alphabet[total[counter]])
 		counter = counter + 1
 
 	finalLetters = ''.join(finalLetters)